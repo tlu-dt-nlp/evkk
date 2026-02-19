ee289ecd91c806e4c05f04afa99c0d6b3380617d
Harli Kodasma <harlikodasma@gmail.com>
2026-02-19 22:25:29 +0200

    ELLE-350 | make wordcloud responsive, move non-base styles out of modal base sheet

--- ui/src/elle/components/ImageDownloadButton.jsx
+++ ui/src/elle/components/ImageDownloadButton.jsx
@@ -78,6 +78,7 @@ export default function ImageDownloadButton({ element, sourceType, fileName }) {
           style={DefaultButtonStyle}
           variant="contained"
           onClick={handleClick}
+          size="small"
         >
           <DownloadIcon fontSize="large" />
         </Button>
