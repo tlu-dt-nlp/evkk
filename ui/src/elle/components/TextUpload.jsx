cb85e346fddc1cd6bb7758487f6487f1a149150f
Harli Kodasma <harlikodasma@gmail.com>
2026-02-27 21:22:39 +0200

    ELLE-350 | refactor TextUpload to use React state handling, modify styles to make it responsive

--- ui/src/elle/components/TextUpload.jsx
+++ ui/src/elle/components/TextUpload.jsx
@@ -12,8 +12,8 @@ export default function TextUpload({ sendTextFromFile }) {
 
   const [modalOpen, setModalOpen] = useState(false);
   const [uploadButtonDisabled, setUploadButtonDisabled] = useState(true);
+  const [selectedFiles, setSelectedFiles] = useState([]);
   const formDataElement = createRef();
-  const fileNameElement = createRef();
   const fileInputRef = createRef();
   const { t } = useTranslation();
   const { getTextFromFile } = useGetTextFromFile();
@@ -23,30 +23,15 @@ export default function TextUpload({ sendTextFromFile }) {
       .then(response => sendTextFromFile(response))
       .catch(() => sendTextFromFile(''));
     setUploadButtonDisabled(true);
-    fileNameElement.current.textContent = '';
+    setSelectedFiles([]);
     setModalOpen(false);
-  }
+  };
 
   const handleFileChange = () => {
-    fileNameElement.current.textContent = '';
-    const br = document.createElement('br');
-    const b = document.createElement('b');
-    const div = document.createElement('div');
-    const fileNameDataContent = document.createTextNode(t('textupload_secondary_modal_chosen_files'));
-    b.appendChild(fileNameDataContent);
-    div.appendChild(b);
-    div.appendChild(br);
-
-    for (let i = 0; i < fileInputRef.current.files.length; i++) {
-      const brElement = document.createElement('br');
-      const tempName = document.createTextNode(fileInputRef.current.files[i].name);
-      div.appendChild(tempName);
-      div.appendChild(brElement);
-    }
-
-    setUploadButtonDisabled(false);
-    fileNameElement.current.appendChild(div);
-  }
+    const files = Array.from(fileInputRef.current.files);
+    setSelectedFiles(files.map(file => file.name));
+    setUploadButtonDisabled(files.length === 0);
+  };
 
   return (
     <>
@@ -55,13 +40,7 @@ export default function TextUpload({ sendTextFromFile }) {
           title={t('textupload_secondary_modal_tooltip')}
           placement="top"
         >
-          <FileUploadIcon
-            id="upload_button"
-            className="button-file"
-            onClick={() => {
-              setModalOpen(true);
-            }}
-          />
+          <FileUploadIcon onClick={() => setModalOpen(true)} />
         </Tooltip>
         <ModalBase
           isOpen={modalOpen}
@@ -85,20 +64,31 @@ export default function TextUpload({ sendTextFromFile }) {
                   {t('textupload_secondary_modal_choose_files')}
                 </Button>
               </div>
-              <div
-                id="file_name"
-                className="file-name-element"
-                ref={fileNameElement}
-              />
+              <div className="file-name-container">
+                {selectedFiles.length > 0 && (
+                  <>
+                    <div className="chosen-files-title">
+                      {t('textupload_secondary_modal_chosen_files')}
+                    </div>
+                    {selectedFiles.map((fileName, index) => (
+                      <div
+                        key={index}
+                        className="file-name"
+                      >
+                        {fileName}
+                      </div>
+                    ))}
+                  </>
+                )}
+              </div>
               <Button
                 sx={DefaultButtonStyle}
                 type="button"
                 variant="contained"
-                onClick={() => {
-                  setModalOpen(false);
-                }}
+                onClick={() => setModalOpen(false)}
                 disabled={uploadButtonDisabled}
-                onMouseDown={handleFileUpload}>
+                onMouseDown={handleFileUpload}
+              >
                 {t('textupload_secondary_modal_upload')}
               </Button>
               <input
